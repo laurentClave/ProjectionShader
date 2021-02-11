51d9cca2ff589e89f7cbb38e858cb6419a43a2d7
laurentExtende <laurent.clave@extende.com>
2021-02-11 10:33:59 +0100

    First comit

---
+++ Assets/SourceOrigin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class SourceOrigin : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _sourceOrigin;
+    [SerializeField]
+    private float _angle = 30;
+    [SerializeField]
+    private Material _projectionMaterial;
+
+    void Update()
+    {
+        if(_projectionMaterial != null && _sourceOrigin != null)
+        {
+            _projectionMaterial.SetFloat("Vector1_60695683", _angle);
+            _projectionMaterial.SetVector("Vector3_C12940D2", _sourceOrigin.transform.position);
+        }
+    }
+}
